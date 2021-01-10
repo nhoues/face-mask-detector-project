e8ff9d829a2d592ce15b1f6e0abda278930b6ed3
Naim Houes <naim@fingenesis.com>
2021-01-10 15:21:00 +0100

    added web_app

---
+++ web_app.py
@@ -0,0 +1,41 @@
+
+from flask import Flask, render_template, Response, jsonify
+import cv2
+
+app = Flask(__name__)
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()        
+
+    def get_frame(self):
+        ret, frame = self.video.read()
+
+        # DO WHAT YOU WANT WITH TENSORFLOW / KERAS AND OPENCV
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+
+        return jpeg.tobytes()
+    
+video_stream = VideoCamera()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(video_stream),
+                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True,port="5000")
